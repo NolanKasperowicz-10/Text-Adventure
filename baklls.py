87d4a176fde367f6bf331f1bd046a97e3c8a4652
Nolan Kasperowicz <johndoe@example.com>
2022-10-04 20:50:32 -0500

    many things

--- baklls.py
+++ baklls.py
@@ -1,5 +1,12 @@
 import re
 #takes the word you inputted
-my_string = input()
+lettercount = input()
 #prints a the number of times a given char occurs in the word
-print(len(re.findall(input(), my_string)))
+print(len(re.findall(input(), lettercount)))
+
+#it can also take any number of letters and numbers in an order and find them
+#for instance, I can ask it how many times "12a" occurs in a string
+
+def letterCount():
+  lettercount = input():
+  print(len(re.findall(input(), letterCount)))
